9e880f2cdaef055aa52e6cd744495bd401eb5073
ks-ampangkl <kugaann@graduate.utm.my>
2026-06-09 20:32:00 +0800

    Second commit

--- public/index.php
+++ public/index.php
@@ -1,9 +1,13 @@
 <?php
+use Dotenv\Dotenv;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
+
+Dotenv::createImmutable(__DIR__ . '/..')->safeLoad(); 
+
 
 $app = AppFactory::create();
 
--- src/routes.php
+++ src/routes.php
@@ -1,27 +1,21 @@
-<?php
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\App;
-use App\Controllers\BookController;
-
-return function (App $app): void {
+<?php 
+use App\Controllers\BookController; 
+use App\Database; 
+use App\Repositories\BookRepository; 
+use Slim\App; 
+  
+return function (App $app): void { 
+    $controller = new BookController(new BookRepository(Database::get())); 
+  
+    $app->get('/', [$controller, 'healthCheck']);
     
-    // Task 13: Refactored clean root health check route
-    $app->get('/', [BookController::class, 'healthCheck']);
+    $app->group('/api', function ($g) use ($controller) { 
+        $g->get   ('/books',       [$controller, 'index']); 
+        $g->get   ('/books/{id}',  [$controller, 'show']); 
+        $g->post  ('/books',       [$controller, 'create']); 
+        $g->put   ('/books/{id}',  [$controller, 'update']); 
+        $g->delete('/books/{id}',  [$controller, 'delete']); 
 
-    // The entire API group MUST live inside this wrapper function
-    $app->group('/api', function ($g) {
-        $g->get('/books', [BookController::class, 'index']);
-        $g->get('/books/{id}', [BookController::class, 'show']);
-        $g->post('/books', [BookController::class, 'create']);
-        $g->put('/books/{id}', [BookController::class, 'update']);
-        $g->delete('/books/{id}', [BookController::class, 'delete']);
-        $g->post('/reset', [BookController::class, 'reset']); 
-
-    $g->options('/{routes:.+}', function ($request, $response) {
-        return $response; // Just return an empty 200 response; the middleware handles the headers!
-    });        
-    });
-
-
-}; // This brace safely closes the return function at the very end
+        $g->options('/{routes:.+}', function ($request, $response) { return $response; });
+    }); 
+}; 
